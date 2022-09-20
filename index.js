500261a4db76e3c858455ff42064fd52a1274afd
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-09-20 16:11:15 +0600

    added file after data loaded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,33 +11,46 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzi1i.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hlzouqs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db user')
-    // perform actions on the collection object
-    client.close();
-});
 
 
-// async function run() {
-//     try {
-//         await client.connect();
-//         const noteCollection = client.db('job_task').collection('notes');
+async function run() {
+    try {
+        await client.connect();
+        const noteCollection = client.db('update_job_task').collection('notes');
 
-//         app.post('/notes', async (req, res) => {
-//             const newNotes = req.body;
-//             const result = await noteCollection.insertOne(newNotes);
-//             res.send(result);
-//         });
+        app.get('/notes', async (req, res) => {
+            const userNote = await noteCollection.find().toArray();
+            res.send(userNote);
+        });
 
-//     }
-//     finally {
+        app.post('/notes', async (req, res) => {
+            const newNotes = req.body;
+            const result = await noteCollection.insertOne(newNotes);
+            res.send(result);
+        });
 
-//     }
-// }
-// run().catch(console.dir);
+        app.get('/notes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const notes = await noteCollection.findOne(query);
+            res.send(notes);
+        });
+
+        app.delete('/notes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await noteCollection.deleteOne(query);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
